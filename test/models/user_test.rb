70751ecae9d134538ac39a0deb924698a4291e1a
Ollie Atkins <ollieatkins8@outlook.com>
2025-01-03 09:41:46 +0000

    Add name and email validations to User

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "requires a name" do
+    @user = User.new(name: "", email: "ollie@example.com")
+    assert_not @user.valid?
+  end
+
+  test "requires a valid email" do
+    @user = User.new(name: "ollie", email: "")
+    assert_not @user.valid?
+
+    @user.email = "invalid"
+    assert_not @user.valid?
+
+    @user.email = "ollie@example.com"
+    assert @user.valid?
+  end
 end
