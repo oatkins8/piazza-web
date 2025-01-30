81d18170fb00f5ae104c50fc0cf3a9b14be2dcbd
Ollie Atkins <ollieatkins8@outlook.com>
2025-01-30 08:23:33 +0000

    Add method to convert session data to hash
    The Session model hashes the token (for security reasons) Hashed tokens
    cannot be queried directly in the database. Instead, the app retrieves the
    session record using its ID, then verifies the token.
    
    Session needs to contain:
       - User ID
       - Session ID
       - Session token
    
    Which is all available to the Session model.
    
    Use user.id rather than user_id. Better to load the user association to
    catch early if the user is nil. Could also use self.token here to prevent
    any confusion with a potential method override or local variable named
    "token".

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   include Authentication
+
+  # 1 method to store a users data in an encrypted cookie
+  # need to contain:
+    # - User ID
+    # - Session ID
+    # - Session token
+
+  
 end
--- app/models/session.rb
+++ app/models/session.rb
@@ -2,4 +2,12 @@ class Session < ApplicationRecord
   has_secure_token
   
   belongs_to :user
+
+  def to_h
+    {
+      user_id: user.id,
+      session_id: id,
+      session_token: token
+    }
+  end
 end
