5b774e5e1efe4e87fb7097361ae22f886e523a68
Ollie Atkins <ollieatkins8@outlook.com>
2025-02-03 13:52:09 +0000

    Add authentication methods to Authentication concern
    ** #authenticate **
    
    Assigns `Current.session` and `Current.user` if the user is successfully
    authenticated. If more methods of authentication need to be added, they
    can with a logical OR operator:
    
      def authenticate
        Current.session = authenticate_using_cookie ||
          authenticate_using_token
    
        Current.user = Current.session&.user
      end
    
    ** #authenticate_using_cookie **
    
    Extracts the data from the encrypted cookie and passes to the
    `authenticate_using method` method. It converts the session to a hash
    with indifferent access. This means values can be retrieved using both
    the symbol and string versions of the key.
    
    ** #authenticate_using **
    
    First line deconstructs the hash into variables using the Ruby 3's rightward
    assignment syntax (fancy!).
    
    If the hash cannot be deconstructed using the symbols specified, a
    NoMatchingPatternError is thrown, essentially meaning authentication was
    unsuccessful. Error rescued and nil returned.
    
    Finally, user object used to authenticate the session
    
    user.authenticate_session(session, token)
    
    which currently will not work as there is no #authenticate_session
    method on user (to be added next!).

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -8,4 +8,25 @@ module Authentication
       value: session.to_h
     }
   end  
+
+  private
+
+  def authenticate
+    Current.session = authenticate_using_cookie
+    Current.user = Current.session&.user
+  end
+
+  def authenticate_using_cookie
+    session = cookies.encrypted[:session]
+    authenticate_using(session&.with_indifferent_access)
+  end
+
+  def authenticate_using(data)
+    data => {user_id:, session:, token:}
+
+    user = User.find(user_id)
+    user.authenticate_session(session, token)
+  rescue NoMatchingPatternError, ActiveRecord::RecordNotFound
+    nil
+  end
 end
